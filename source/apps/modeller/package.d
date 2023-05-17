4fc70afbe4067c159ab3ca1d7b743cfd84fd497d
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:32:24 +0200

    Update

--- source/apps/modeller/package.d
+++ source/apps/modeller/package.d
@@ -1,1 +1,35 @@
 
+module apps.modeller;
+
+mixin(ImportPhobos!());
+
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.modeller.controllers;
+  import apps.modeller.helpers;
+  import apps.modeller.tests;
+  import apps.modeller.views;
+}
+
+DApp modellerApp;
+static this() {
+  modellerApp = App
+    .name("modellerApp")
+    .rootPath("/apps/modeller")
+    .addRoute(Route("", HTTPMethod.GET, MODIndexPageController));
+    .addRoute(Route("/", HTTPMethod.GET, MODIndexPageController));
+}
